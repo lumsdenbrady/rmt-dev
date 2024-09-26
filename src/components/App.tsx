7ac07b339520143589eb572762740fcf28a7f59d
Brady Lumsden <blumsden007@gmail.com>
2024-09-26 07:51:27 -0700

    create type file, make search function fetch data. create loading state. Next: implement loading state, display returned data from fetch.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,31 +4,55 @@ import Background from "./Background";
 import Container from "./Container";
 import Footer from "./Footer";
 import Header from "./Header";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import SearchForm from "./SearchForm";
+import { jobItemBasic } from "../types";
 
 function App() {
   // jobItem list logic
-  const [jobItems, setJobItems] = useState<string[]>([]);
+  const [jobItems, setJobItems] = useState<jobItemBasic[]>([]);
+  const [isLoading, setIsLoading] = useState(false)
 
 //handle search text logic
 const [searchText, setSearchText] = useState("");
+useEffect(()=>{
+  console.log(searchText)
+  fetchJobItems(searchText)
+
+
+},[searchText])
 const handleSearchChange = (e:React.ChangeEvent<HTMLInputElement>) =>{
   e.preventDefault();
   setSearchText(e.target.value)
-  fetchJobItems(searchText)
 }
 
   // here is where we fetch data from the api based on the search text.
 
-  const fetchJobItems = async(searchText:string): Promise<string[]>=>{
+  const fetchJobItems = async(searchText:string)=>{
+    setIsLoading(true)
+    try{
 const res = await fetch(`${BASE_URL}?search=${searchText}`);
 const data = await res.json();
-console.log(data);
-setJobItems([])
+console.log(data.jobItems);
+if (!res.ok){
+  console.log(data.description);
+  setIsLoading(false)
 
-return []
+  return;  
+}
+if (!data.jobItems) {
+  setIsLoading(false)
+  return}
+else {
+  setIsLoading(false)
+setJobItems(()=>[...data.jobItems] as jobItemBasic[])
+} 
   }
+  catch (error){
+    console.log(error);
+    setIsLoading(false)
+  }
+}
 
 
   return (
